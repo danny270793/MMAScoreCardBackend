ef3073611cb16fbf0eac6cac02a0f3dfe2505394
Danny Vaca <danny270793@icloud.com>
2025-04-07 16:57:40 -0500

    model for fight

---
+++ backend/app/Models/Fight.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Fight extends Model
+{
+    protected $fillable = [
+        'position',
+        'event_id',
+        'fighter1_id',
+        'fighter1_result',
+        'fighter2_id',
+        'fighter2_result',
+        'division_id',
+        'method',
+        'referee_id',
+        'round',
+        'time',
+    ];
+
+    public function event(): BelongsTo
+    {
+        return $this->belongsTo(Event::class);
+    }
+
+    public function fighter1(): BelongsTo
+    {
+        return $this->belongsTo(Fighter::class, 'fighter1_id');
+    }
+
+    public function fighter2(): BelongsTo
+    {
+        return $this->belongsTo(Fighter::class, 'fighter2_id');
+    }
+
+    public function division(): BelongsTo
+    {
+        return $this->belongsTo(Division::class);
+    }
+
+    public function referee(): BelongsTo
+    {
+        return $this->belongsTo(Referee::class);
+    }
+}
