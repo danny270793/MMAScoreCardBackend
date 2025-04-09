f3c38b2af253d27bcbc8ad9c274629c22dc920c6
Danny Vaca <danny270793@icloud.com>
2025-04-09 18:06:00 -0500

    controller to seee fighters

---
+++ backend/app/Http/Controllers/FightersController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Fighter;
+use App\Models\Fight;
+
+class FightersController extends Controller
+{
+    function index()
+    {
+        $fighters = Fighter::paginate(10);
+        return response()
+            ->json($fighters);
+    }
+    function get($id)
+    {
+        $fighter = Fighter::find($id);
+        return response()
+            ->json($fighter);
+    }
+    function fightsByFighter($id)
+    {
+        $fights = Fight::where('fighter1_id', $id)
+            ->orWhere('fighter2_id', $id)
+            ->with('fighter1', 'fighter2', 'division', 'referee')
+            ->paginate(10);
+        return response()
+            ->json($fights);
+    }
+    function search(Request $request)
+    {
+        $query = $request->input('query');
+        $fighters = Fighter::where('name', 'LIKE', "%$query%")
+            ->orWhere('nickname', 'LIKE', "%$query%")
+            ->orWhere('country', 'LIKE', "%$query%")
+            ->orWhere('city', 'LIKE', "%$query%")
+            ->orWhere('weight', 'LIKE', "%$query%")
+            ->paginate(10);
+        return response()
+            ->json($fighters);
+
+    }
+}
