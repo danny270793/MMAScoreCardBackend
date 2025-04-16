44a60d947248efed48b7350bbc61d961503304b8
Danny Vaca <danny270793@icloud.com>
2025-04-16 08:06:43 -0500

    entrypoint

---
+++ frontend/src/main.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+// import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { EventsPage } from './pages/events'
+import { EventPage } from './pages/event'
+import { store } from './reducers'
+import { FightPage } from './pages/fight'
+import { NotFoundPage } from './pages/not-found'
+import { FighterPage } from './pages/fighter'
+import { HomePage } from './pages/home'
+import { FightersPage } from './pages/fighters'
+import './styles/index.css'
+import './styles/w3css.css'
+
+const rootElement: HTMLElement | null = document.getElementById('root')
+createRoot(rootElement!).render(
+  // <StrictMode>
+  <Provider store={store}>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/events/" element={<EventsPage />} />
+        <Route path="/fighters/" element={<FightersPage />} />
+        <Route path="/events/:id" element={<EventPage />} />
+        <Route path="/fight/:id" element={<FightPage />} />
+        <Route path="/fighter/:id" element={<FighterPage />} />
+        <Route path="/*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
+  </Provider>,
+  // </StrictMode>,
+)
