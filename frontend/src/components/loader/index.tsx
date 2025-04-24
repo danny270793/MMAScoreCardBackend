5d407219ce0a14584bda45130f41711bb8e87a4a
Danny Vaca <danny270793@icloud.com>
2025-04-24 08:27:19 -0500

    add more sises to loader

--- frontend/src/components/loader/index.tsx
+++ frontend/src/components/loader/index.tsx
@@ -1,23 +1,19 @@
 import React from 'react'
 import './index.css'
 
-export type LoaderSize = 'small' | 'normal'
+export type LoaderSize = 'xsmall' | 'small' | 'normal'
 
 export interface LoaderProps {
   size?: LoaderSize
   fullscreen?: boolean
 }
 
-export const DefaultLoaderProps: LoaderProps = {
-  size: 'normal',
-  fullscreen: false,
-}
-
-export const Loader: (props: LoaderProps) => React.ReactElement = (
-  props: LoaderProps = DefaultLoaderProps,
-) => {
+export const Loader: (props: LoaderProps) => React.ReactElement = ({
+  size = 'normal',
+  fullscreen = false,
+}: LoaderProps) => {
   const Wrapper = ({ children }: { children: React.ReactNode }) => {
-    if (props.fullscreen) {
+    if (fullscreen) {
       return (
         <div className="w3-display-container" style={{ height: '100vh' }}>
           <div className="w3-display-middle">{children}</div>
@@ -27,16 +23,17 @@ export const Loader: (props: LoaderProps) => React.ReactElement = (
     return children
   }
 
-  const size: number = props.size === 'small' ? 60 : 120
-  const borderSize: number = props.size === 'small' ? 8 : 16
+  const loaderSize: number =
+    size === 'xsmall' ? 30 : size === 'small' ? 60 : 120
+  const borderSize: number = size === 'xsmall' ? 4 : size === 'small' ? 8 : 16
   return (
     <Wrapper>
       <div className="w3-block w3-center">
         <div
           className="loader w3-bar"
           style={{
-            width: `${size}px`,
-            height: `${size}px`,
+            width: `${loaderSize}px`,
+            height: `${loaderSize}px`,
             border: `${borderSize}px solid #f3f3f3`,
             borderTop: `${borderSize}px solid #3498db`,
           }}
