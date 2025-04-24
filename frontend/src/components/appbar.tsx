3e82ba60bb47d8fb695e6ef8ec0af64140f5cfd3
Danny Vaca <danny270793@icloud.com>
2025-04-24 08:27:28 -0500

    add actions to appbar

--- frontend/src/components/appbar.tsx
+++ frontend/src/components/appbar.tsx
@@ -1,3 +1,5 @@
+import { faAngleLeft } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import {
   Location,
@@ -5,14 +7,17 @@ import {
   useLocation,
   useNavigate,
 } from 'react-router-dom'
+import { Button } from './button'
 
 export interface AppBarProps {
+  actions?: React.ReactElement[]
   title: string
 }
 
-export const AppBar: (props: AppBarProps) => React.ReactElement = (
-  props: AppBarProps,
-): React.ReactElement => {
+export const AppBar: (props: AppBarProps) => React.ReactElement = ({
+  actions = [],
+  title,
+}: AppBarProps): React.ReactElement => {
   const navigate: NavigateFunction = useNavigate()
   const location: Location = useLocation()
 
@@ -23,12 +28,18 @@ export const AppBar: (props: AppBarProps) => React.ReactElement = (
     <div>
       <div className="w3-top">
         <div className="w3-blue w3-bar">
-          {!['/', '/index.html'].includes(location.pathname) && (
-            <button className="w3-bar-item w3-blue" onClick={onBackClicked}>
-              {'<'}
-            </button>
+          {!['/', '/index.html', '/login'].includes(location.pathname) && (
+            <Button className="w3-bar-item" onClick={onBackClicked}>
+              <FontAwesomeIcon icon={faAngleLeft} />
+            </Button>
           )}
-          <div className="w3-bar-item">{props.title}</div>
+          <div className="w3-bar-item">{title}</div>
+
+          {actions.map((action: React.ReactElement, index: number) => (
+            <div key={index} className="w3-right ">
+              {action}
+            </div>
+          ))}
         </div>
       </div>
       <br />
