26053ed84aa6ac2277aa7b5df9dadf77087a621d
Danny Vaca <danny270793@icloud.com>
2025-04-25 11:10:57 -0500

    add log to each sagas dispatch and persis store

--- frontend/src/reducers/index.ts
+++ frontend/src/reducers/index.ts
@@ -1,23 +1,57 @@
 import { configureStore, EnhancedStore } from '@reduxjs/toolkit'
-import { combineReducers, Reducer } from 'redux'
+import { combineReducers, Middleware, Reducer, UnknownAction } from 'redux'
 import createSagaMiddleware, { SagaMiddleware } from 'redux-saga'
-import { reducer as backendReducer } from './backend'
+import { reducer as backendReducer, BackendState } from './backend'
+import { reducer as sessionReducer, SessionState } from './session'
 import { rootSagas } from '../sagas'
+import {
+  PersistConfig,
+  Persistor,
+  persistReducer,
+  persistStore,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { Logger } from '../utils/logger'
+
+const logger: Logger = new Logger('/src/reducers/index.ts')
 
 const sagaMiddleware: SagaMiddleware = createSagaMiddleware()
 
-export const rootReducer: Reducer = combineReducers({
+export type RootReducer = { backend: BackendState; session: SessionState }
+
+export const rootReducer: Reducer<RootReducer> = combineReducers({
   backend: backendReducer,
+  session: sessionReducer,
 })
 
-export const store: EnhancedStore = configureStore({
-  reducer: rootReducer,
+const persistConfig: PersistConfig<RootReducer> = {
+  key: 'root',
+  storage,
+  whitelist: ['session'],
+}
+
+const persistedReducer: Reducer = persistReducer<RootReducer>(
+  persistConfig,
+  rootReducer,
+)
+
+const storeLogger: Middleware = () => (next) => (action) => {
+  if (import.meta.env.MODE === 'development') {
+    logger.debug(`Dispatching action: ${(action as UnknownAction).type}`)
+  }
+  return next(action)
+}
+
+export const store: EnhancedStore<RootReducer> = configureStore({
+  reducer: persistedReducer,
   devTools: true,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       thunk: false,
       serializableCheck: false,
-    }).concat(sagaMiddleware),
+    }).concat(sagaMiddleware, storeLogger),
 })
 
 sagaMiddleware.run(rootSagas)
+
+export const persistedStore: Persistor = persistStore(store)
