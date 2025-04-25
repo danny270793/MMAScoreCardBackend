888d0e23e9292f136da7bc325212fd4ff5e00ef5
Danny Vaca <danny270793@icloud.com>
2025-04-25 11:12:30 -0500

    improv ecordova typing

--- frontend/src/types/cordova.ts
+++ frontend/src/types/cordova.ts
@@ -1,9 +1,24 @@
 interface Cordova {
-  platformId: 'android'|'ios'
+  platformId: 'android' | 'ios' | 'electron'
+}
+interface Device {
+  cordova: string
+  available: boolean
+
+  platform: string
+  model: string
+  version: string
+
+  manufacturer: string
+  uuid: string
+  isVirtual: boolean
+  serial: string
+  sdkVersion?: string
 }
 
 declare global {
   interface Window {
     cordova: Cordova | undefined
+    device: Device | undefined
   }
 }
