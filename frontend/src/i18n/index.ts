93ccba9786d97fc7d7d6230b8cafbebbb13562f1
Danny Vaca <danny270793@icloud.com>
2025-04-26 16:08:31 -0500

    i18n configuration

---
+++ frontend/src/i18n/index.ts
@@ -0,0 +1,29 @@
+import i18Next from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import { en, es } from './langs'
+import { capitalize } from '../utils/strings'
+
+export const i18n = i18Next
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .use({
+    type: 'postProcessor',
+    name: 'capitalize',
+    process: capitalize,
+  })
+  .init({
+    resources: {
+      en: {
+        translation: en,
+      },
+      es: {
+        translation: es,
+      },
+    },
+    fallbackLng: 'en',
+    debug: true,
+    interpolation: {
+      escapeValue: false,
+    },
+  })
