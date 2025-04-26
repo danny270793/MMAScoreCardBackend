842e1ad8331b60b9c90ae4ff0e4db9f9e1d70899
Danny Vaca <danny270793@icloud.com>
2025-04-26 16:08:13 -0500

    strings

---
+++ frontend/src/i18n/langs/index.ts
@@ -0,0 +1,16 @@
+export interface Lang {
+  devices: string
+  devicesNotFound: string
+  confirmDeleteTitle: string
+  confirmDeleteMessage: string
+  yes: string
+  no: string
+  appName: string
+  events: string
+  fighters: string
+  logout: string
+  settings: string
+}
+
+export * from './en'
+export * from './es'
