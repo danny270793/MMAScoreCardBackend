0cab0a3e3d04c5ff83797b535705e24913fcda51
Danny Vaca <danny270793@icloud.com>
2025-04-26 16:07:55 -0500

    english strings

---
+++ frontend/src/i18n/langs/en.ts
@@ -0,0 +1,15 @@
+import { Lang } from '.'
+
+export const en: Lang = {
+  devices: 'devices',
+  confirmDeleteMessage: 'are you sure you want to delete?',
+  appName: 'UFC Events',
+  devicesNotFound: 'no devices found',
+  confirmDeleteTitle: 'Delete',
+  yes: 'yes',
+  no: 'no',
+  events: 'events',
+  fighters: 'fighters',
+  logout: 'logout',
+  settings: 'settings',
+}
