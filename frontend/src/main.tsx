c974123b1984489afae7c81621e8f14d5cd1ed94
Danny Vaca <danny270793@icloud.com>
2025-04-27 13:18:33 -0500

    add new features

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,19 +5,43 @@ import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from './reducers'
+import { persistedStore, store } from './reducers'
 import './styles/index.css'
 import './styles/w3css.css'
-import { routes } from './pages'
+import './i18n'
+import { Routes } from './pages'
+import { PersistGate } from 'redux-persist/integration/react'
+import { Loader } from './components/loader'
+import { ErrorBoundary } from './components/error-boundary'
+import React from 'react'
+import { ThemeContextProvider } from './context/theme-context'
 
 function startupReact() {
   const rootElement: HTMLElement | null = document.getElementById('root')
+
+  const Router: React.FC<{ children: React.ReactNode }> = (props) => {
+    if (window.cordova) {
+      return <HashRouter>{props.children}</HashRouter>
+    }
+    return <BrowserRouter>{props.children}</BrowserRouter>
+  }
+
   createRoot(rootElement!).render(
     // <StrictMode>
-    <Provider store={store}>
-      {window.cordova && <HashRouter>{routes}</HashRouter>}
-      {!window.cordova && <BrowserRouter>{routes}</BrowserRouter>}
-    </Provider>,
+    <ErrorBoundary>
+      <ThemeContextProvider>
+        <Provider store={store}>
+          <PersistGate
+            persistor={persistedStore}
+            loading={<Loader fullscreen={true} />}
+          >
+            <Router>
+              <Routes />
+            </Router>
+          </PersistGate>
+        </Provider>
+      </ThemeContextProvider>
+    </ErrorBoundary>,
     // </StrictMode>,
   )
 }
