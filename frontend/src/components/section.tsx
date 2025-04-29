f20583ba2cae35953667036158a51796e3d97181
Danny Vaca <danny270793@icloud.com>
2025-04-29 12:01:37 -0500

    add classes to section

--- frontend/src/components/section.tsx
+++ frontend/src/components/section.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 export interface SectionProps {
+  className?: string
   children: React.ReactNode
 }
 
 export const Section = (props: SectionProps): React.ReactElement => {
-  return <div className="w3-padding">{props.children}</div>
+  return (
+    <div className={['w3-padding', props.className].join(' ')}>
+      {props.children}
+    </div>
+  )
 }
