b44a110a53771d1c992ae503b93456cd3ada2628
Danny Vaca <danny270793@icloud.com>
2025-04-29 12:02:05 -0500

    loader

--- frontend/src/components/with-loader.tsx
+++ frontend/src/components/with-loader.tsx
@@ -3,14 +3,17 @@ import { Loader } from './loader'
 
 export interface WithLoaderProps {
   isLoading: boolean
-  children: React.ReactElement | React.ReactElement[] | null
+  children: React.ReactNode
 }
 
 export const WithLoader = (props: WithLoaderProps): React.ReactElement => {
   return (
     <>
       {props.isLoading && <Loader fullscreen={true} />}
-      <div style={{ visibility: props.isLoading ? 'hidden' : 'visible' }}>
+      <div
+        className={props.isLoading ? '' : 'w3-animate-right'}
+        style={{ visibility: props.isLoading ? 'hidden' : 'visible' }}
+      >
         {props.children}
       </div>
     </>
