d206e5747d0dbed830ae346dfb04a9c44932d83b
Danny Vaca <danny270793@icloud.com>
2025-04-30 09:48:45 -0500

    improve router

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes as RouterRoutes } from 'react-router-dom'
 import { HomePage } from './home'
 import { EventsPage } from './events'
 import { FightersPage } from './fighters'
@@ -7,16 +7,44 @@ import { EventPage } from './event'
 import { FightPage } from './fight'
 import { FighterPage } from './fighter'
 import { NotFoundPage } from './not-found'
+import { LoginPage } from './login'
+import { AuthenticatedRoutes } from '../components/authenticated-routes'
+import { DevicesPage } from './devices'
+import { DevicePage } from './device'
+import { SettingsPage } from './settings'
 
-export const routes: React.ReactElement = (
-  <Routes>
-    <Route path="/" element={<HomePage />} />
-    {window.cordova && <Route path="/index.html" element={<HomePage />} />}
-    <Route path="/events/" element={<EventsPage />} />
-    <Route path="/fighters/" element={<FightersPage />} />
-    <Route path="/events/:id" element={<EventPage />} />
-    <Route path="/fight/:id" element={<FightPage />} />
-    <Route path="/fighter/:id" element={<FighterPage />} />
-    <Route path="/*" element={<NotFoundPage />} />
-  </Routes>
-)
+export const RoutesName: { [key: string]: string } = {
+  home: '/',
+  login: '/login',
+  devices: '/devices',
+  device: '/device/:id',
+  events: '/events',
+  event: '/event/:id',
+  fighters: '/fighters',
+  fight: '/fight/:id',
+  fighter: '/fighter/:id',
+  settings: '/settings',
+}
+
+export const Routes: () => React.ReactElement = () => {
+  return (
+    <RouterRoutes>
+      <Route path="/" element={<AuthenticatedRoutes />}>
+        <Route path={RoutesName.home} element={<HomePage />} />
+        {window.cordova && <Route path="/index.html" element={<HomePage />} />}
+
+        <Route path={RoutesName.devices} element={<DevicesPage />} />
+        <Route path={RoutesName.device} element={<DevicePage />} />
+        <Route path={RoutesName.events} element={<EventsPage />} />
+        <Route path={RoutesName.event} element={<EventPage />} />
+        <Route path={RoutesName.fighters} element={<FightersPage />} />
+        <Route path={RoutesName.fight} element={<FightPage />} />
+        <Route path={RoutesName.fighter} element={<FighterPage />} />
+        <Route path={RoutesName.settings} element={<SettingsPage />} />
+      </Route>
+
+      <Route path={RoutesName.login} element={<LoginPage />} />
+      <Route path="/*" element={<NotFoundPage />} />
+    </RouterRoutes>
+  )
+}
