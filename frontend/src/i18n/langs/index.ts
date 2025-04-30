99dc359cc1f0ddacd3e8b786b2293744ae7d682b
Danny Vaca <danny270793@icloud.com>
2025-04-30 09:46:12 -0500

    add new strings

--- frontend/src/i18n/langs/index.ts
+++ frontend/src/i18n/langs/index.ts
@@ -8,8 +8,36 @@ export interface Lang {
   appName: string
   events: string
   fighters: string
+  fighter: string
+  fightersNotFound: string
   logout: string
   settings: string
+  eventsNotFound: string
+  fightsNotFound: string
+  loadMore: string
+  fight: string
+  win: string
+  loss: string
+  event: string
+  fights: string
+  draw: string
+  nc: string
+  stats: string
+  record: string
+  octagonTime: string
+  currentStreak: string
+  bestStreak: string
+  worstStreak: string
+  login: string
+  email: string
+  password: string
+  goToHome: string
+  pageNotFound: string
+  dark: string
+  light: string
+  system: string
+  noTheme: string
+  theme: string
 }
 
 export * from './en'
