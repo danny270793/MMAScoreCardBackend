2e8fe571648e95ea4a372029c2abd3dbee311550
Danny Vaca <danny270793@icloud.com>
2025-05-06 10:52:40 -0500

    use typescript for config

---
+++ capacitor/capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: "io.github.danny270793.ufcevents",
+  appName: "UFC Events",
+  webDir: "www",
+}
+
+export default config;
